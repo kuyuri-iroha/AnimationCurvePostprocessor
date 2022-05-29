476b4cae3229fdd0e26a68e976522a393c914077
kuyuri-iroha <kuyuri.iroha@gmail.com>
2022-05-30 03:19:07 +0900

    Steinman補間の実装
    単調性保持できず

--- KeyFrameReductionTool/Assets/Scripts/KeyframeReductionTool.cs
+++ KeyFrameReductionTool/Assets/Scripts/KeyframeReductionTool.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -41,7 +42,6 @@ public class KeyframeReductionTool : AssetPostprocessor
                 
                 Reduction(out var reducedCurve, curve, 0.5f);
                 Smoothing(out var smoothedCurve, reducedCurve);
-                
                 newCurveBindings.Add(binding);
                 newCurves.Add(smoothedCurve);
             }
@@ -111,10 +111,13 @@ public class KeyframeReductionTool : AssetPostprocessor
         var keyframes = sourceCurve.keys;
         DouglasPeucker(out var reducedKeyframes, keyframes, eps);
 
-        reducedCurve = new AnimationCurve(reducedKeyframes);
+        reducedCurve = new AnimationCurve
+        {
+            keys = reducedKeyframes
+        };
     }
     
-    private static void DouglasPeucker(out Keyframe[] outKeyFrames, in Keyframe[] inKeyframes, float eps = 1e-4f)
+    private static void DouglasPeucker(out Keyframe[] outKeyFrames, in Keyframe[] inKeyframes, float eps)
     {
         if (inKeyframes.Length < 3)
         {
@@ -147,7 +150,7 @@ public class KeyframeReductionTool : AssetPostprocessor
             DouglasPeucker(out var resKeys1, in keys1, eps);
             DouglasPeucker(out var resKeys2,in keys2, eps);
             
-            outKeyFrames = resKeys1.Concat(resKeys2).ToArray();
+            outKeyFrames = resKeys1.Concat(resKeys2).DistinctBy(key => key.time).ToArray();
         }
         else
         {
@@ -195,17 +198,58 @@ public class KeyframeReductionTool : AssetPostprocessor
     private static void Smoothing(out AnimationCurve smoothedCurve, AnimationCurve sourceCurve, float eps = 0.0001f)
     {
         smoothedCurve = new AnimationCurve();
-        foreach (var keyframe in sourceCurve.keys)
+        var sourceKeyLength = sourceCurve.keys.Length;
+        
+        if(sourceKeyLength < 3)
+        {
+            smoothedCurve.keys = sourceCurve.keys.ToArray();
+            return;
+        }
+        
+        var x = new float[sourceKeyLength];
+        var y = new float[sourceKeyLength];
+        var xi = new float[sourceKeyLength * 2 - 1];
+        for(var i = 0; i < sourceKeyLength - 1; i++)
+        {
+            x[i] = sourceCurve.keys[i].time;
+            y[i] = sourceCurve.keys[i].value;
+            xi[i * 2] = sourceCurve.keys[i].time;
+            xi[i * 2 + 1] = (sourceCurve.keys[i + 1].time + sourceCurve.keys[i].time) * 0.5f;
+        }
+        x[sourceKeyLength - 1] = sourceCurve.keys[sourceKeyLength - 1].time;
+        y[sourceKeyLength - 1] = sourceCurve.keys[sourceKeyLength - 1].value;
+        xi[(sourceKeyLength - 1) * 2] = x[sourceKeyLength - 1];
+
+        StinemanInterpolation(out var yi, x, y, xi);
+        
+        /*
+        // Tangentでスムージング
+        for (var i = 0; i < sourceKeyLength; i++)
+        {
+            var interpIndex = i * 2;
+            var inTangent = 0 < i ? (yi[interpIndex] - yi[interpIndex - 1]) / (xi[interpIndex] - xi[interpIndex - 1]) : 0;
+            var outTangent = i < sourceKeyLength - 1 ? (yi[interpIndex + 1] - yi[interpIndex]) / (xi[interpIndex + 1] - xi[interpIndex]) : 0;
+            smoothedCurve.AddKey(new Keyframe()
+            {
+                time = sourceCurve.keys[i].time,
+                value = sourceCurve.keys[i].value,
+                inTangent = inTangent,
+                outTangent = outTangent,
+                inWeight = 0,
+                outWeight = 0,
+            });
+        }
+        */
+        for (var i = 0; i < yi.Length; i++)
         {
             smoothedCurve.AddKey(new Keyframe()
             {
-                time = keyframe.time,
-                value = keyframe.value,
-                inTangent = keyframe.inTangent,
-                outTangent = keyframe.outTangent,
-                inWeight = keyframe.inWeight,
-                outWeight = keyframe.outWeight,
-                weightedMode = WeightedMode.Both,
+                time = xi[i],
+                value = yi[i],
+                inTangent = 0,
+                outTangent = 0,
+                inWeight = 0,
+                outWeight = 0,
             });
         }
 
@@ -216,7 +260,65 @@ public class KeyframeReductionTool : AssetPostprocessor
             AnimationUtility.SetKeyBroken(smoothedCurve, i, true);
         }
     }
-    
+
+    // Monotone cubic interpolationの手法の１つであるStineman法の補間
+    private static void StinemanInterpolation(out float[] yi, float[] x, float[] y, float[] xi, float[] yp = null)
+    {
+        if (x.Length != y.Length)
+        {
+            yi = null;
+            return;
+        }
+
+        // 各点の傾きを事前計算
+        var dx = new float[x.Length - 1];
+        var dy = new float[x.Length - 1];
+        var dydx = new float[x.Length - 1];
+        for(var i = 0; i < x.Length - 1; i++)
+        {
+            dx[i] = x[i + 1] - x[i];
+            dy[i] = y[i + 1] - y[i];
+            dydx[i] = dy[i] / dx[i];
+        }
+        
+        // 元データの各点の傾きの算出
+        if (yp == null)
+        {
+            yp = new float[x.Length];
+            for (var i = 1; i < x.Length - 1; i++)
+            {
+                yp[i] = (dydx[i - 1] * dx[i] + dydx[i] * dx[i - 1]) / (dx[i] + dx[i - 1]);
+            }
+            yp[0] = 2.0f * dy[0] / dx[0] - yp[1];
+            yp[^1] = 2.0f * dy[^1] / dx[^1] - yp[^2];
+        }
+        
+        // yiの算出
+        yi = new float[xi.Length];
+        for (var i = 0; i < xi.Length; i++)
+        {
+            var xii = xi[i];
+            var index = Array.BinarySearch(x, xii);
+            index = Mathf.Max((index < 0 ? ~index : index) - 1, 0);
+
+            var baseDydx = dydx[index];
+            var baseX = x[index];
+            var baseY = y[index];
+            var nextX = x[index + 1];
+            var yo = baseY + baseDydx * (xii - baseX);
+
+            var dy1 = (yp[index] - baseDydx) * (xii - baseX);
+            var dy2 = (yp[index + 1] - baseDydx) * (xii - nextX);
+            var dy1dy2 = dy1 * dy2;
+
+            yi[i] = yo + dy1dy2 * Math.Sign(dy1dy2) switch {
+                -1 => (2.0f * xii - baseX - nextX) / ((dy1 - dy2) * (nextX - baseX)),
+                0 => 0.0f,
+                _ => 1.0f / (dy1 + dy2)
+            };
+        }
+    }
+
     // アニメーションクリップの保存
     private static void WriteAnimationCurve(AnimationClip animClip, string animClipDirectory, string animClipName)
     {
